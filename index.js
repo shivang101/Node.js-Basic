3c830c03262f118a8a927aea6c164d732de8a31e
shivang <shivangmathur101@gmail.com>
2021-12-22 13:55:02 +0530

    Asynchronous Reading and Writing Files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //how to require a module
 const fs = require("fs");
+/*
 //fs stands for file system used for
 //reading and writing in files
 
@@ -21,3 +22,24 @@ const textOut = `this is what we know about the avocado ${textIn}.\nCreated on $
 fs.writeFileSync("./txt/output.txt", textOut);
 
 console.log("File Written");
+*/
+///////////////////////////////////////////////////////////////////
+//Non-Blocking, asynchronous way
+
+// fs.readFile("path","file_encoding",callBackFuntion)
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("Error 💥💥");
+
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+
+      //for writing file (path,data,encoding,function)
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file has been written");
+      });
+    });
+  });
+});
+console.log("Reading the file");
