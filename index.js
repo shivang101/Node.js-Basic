98d40b1cf32c5d5264f3313cc9a9c632f4e464ba
shivang <shivangmathur101@gmail.com>
2021-12-24 17:06:58 +0530

    placeholders added

--- index.js
+++ index.js
@@ -1,56 +1,18 @@
-//how to require a module
 const fs = require("fs");
-//fs stands for file system used for
-//reading and writing in files
-
 const http = require("http");
 const url = require("url");
 
-//FILES
-/*
-// fs.readFileSync(path,encoding)
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-//Sync stands for synchronous(BLOCKING CODE)
-//Each statement is processed one after another
-//We should use Asynchronous code
-console.log(textIn);
-
-const today = new Date();
-const year = String(today.getFullYear());
-const month = String(today.getMonth() + 1).padStart(2, 0);
-const date = String(today.getDate()).padStart(2, 0);
-const hours = String(today.getHours()).padStart(2, 0);
-const mins = String(today.getMinutes()).padStart(2, 0);
-
-const textOut = `this is what we know about the avocado ${textIn}.\nCreated on ${date}/${month}/${year}\ntime was ${hours}:${mins}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-
-console.log("File Written");
-
-///////////////////////////////////////////////////////////////////
-//Non-Blocking, asynchronous way
-
-// fs.readFile("path","file_encoding",callBackFuntion)
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) return console.log("Error 💥💥");
-
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
-
-      //for writing file (path,data,encoding,function)
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("Your file has been written");
-      });
-    });
-  });
-});
-console.log("Reading the file");
-*/
 //////////////////////////////////////
 //SERVER
 //
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data); //json to javascript here array of objects(string to object)
 
@@ -61,11 +23,19 @@ const server = http.createServer((req, res) => {
   //Routing
   const pathName = req.url;
 
+  //Overview Page
   if (pathName === "/overview" || pathName === "/") {
-    res.end("This is the overview Page");
-  } else if (pathName === "/product") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+    res.end(tempOverview);
+  }
+  //Product Page
+  else if (pathName === "/product") {
     res.end("This is the Product page");
-  } else if (pathName === "/api") {
+  }
+  //API
+  else if (pathName === "/api") {
     res.writeHead(200, {
       "Content-type": "application/json",
     });
