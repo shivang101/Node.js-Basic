d2051caded8f89e257bc7defa91d2692b829fbaf
shivang <shivangmathur101@gmail.com>
2021-12-25 14:52:56 +0530

    made our module and exported it

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
+//core modules
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-
+//own modules
+const moduleFunction = require("./Modules/replaceTemplate");
 //////////////////////////////////////
 //SERVER
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-
-  return output;
-};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -55,7 +41,7 @@ const server = http.createServer((req, res) => {
   //REDIRECTING PAGES
   else if (pathname === "/about") {
     const cardsHtml = dataObj
-      .map((el) => replaceTemplate(tempCard, el))
+      .map((el) => moduleFunction(tempCard, el))
       .join("");
 
     const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
@@ -66,7 +52,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/html" });
 
     const product = dataObj[query.id];
-    const output = replaceTemplate(tempProduct, product);
+    const output = moduleFunction(tempProduct, product);
     res.end(output);
   }
   //API
